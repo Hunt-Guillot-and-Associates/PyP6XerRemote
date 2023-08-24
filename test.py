3bba96d0c0dc41b58a43c6f53827f554faf2b0fa
Andrew Eitel <andreweitel1@gmail.com>
2023-08-24 08:18:48 -0500

    first commit

---
+++ test.py
@@ -0,0 +1,52 @@
+from pprint import pprint
+from xerparser.reader import Reader
+from xerparser import *
+import urllib.request
+from urllib.request import urlopen
+import io
+from xerparser.reader import Reader
+
+
+
+
+urlval = "https://pgmuno.s3.amazonaws.com/media/files/sitewideupload/08-23-2023/hwcdb/AMOGY_B-5_23Aug.xer"
+
+xer = Reader(urlval)
+
+
+# BUILD ACT REFERENCE
+act_ref = {}
+for act in xer.activities:
+    act_ref[act.id] = act
+
+for prj in xer.projects:
+    for wbs in prj.wbss:
+        print(f"WBS: {wbs}")
+
+        # order by start date
+        act_list = wbs.activities
+        act_list.sort(key=lambda x: x.start_date)
+
+        print('')
+
+        for act in act_list:
+            dict_ref = act.get_tsv_dict()
+            print("    Activity ID ", act.id ,"| ", act.task_code, " | driving_path_flag | ", dict_ref['driving_path_flag'], " | Pred | ", f"Start Date: {act.start_date} | End Date: {act.end_date} ", " | Activity Name | ", act.task_name, )
+
+            print('')
+            for pred in xer.relations.get_predecessors(act.id):
+                pred_vals = pred.get_tsv()
+                sub_act = act_ref[pred_vals[3]]
+
+                print(f'        PRED | ACTIVITY ID {sub_act.task_code} | ACTIVITY NAME {sub_act.task_name} | RELATION TYPE {pred_vals[6]} | LAG {pred_vals[7]}')
+                print('')
+
+
+            for succ in xer.relations.get_successors(act.id):
+                succ_vals = succ.get_tsv()
+                sub_act = act_ref[succ_vals[3]]
+
+                print(f'        SUCC | ACTIVITY ID {sub_act.task_code} | ACTIVITY NAME {sub_act.task_name} | RELATION TYPE {pred_vals[6]} | LAG {pred_vals[7]}')
+                print('')
+
+
